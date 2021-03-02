57cb055c442584135e9f5b0a465d8f885d9db379
Franeb85 <79915750+Franeb85@users.noreply.github.com>
2021-03-02 13:55:51 +0100

    Testing

--- esercizi-stringhe-numeri/conta-il-tempo.js
+++ esercizi-stringhe-numeri/conta-il-tempo.js
@@ -7,6 +7,9 @@
     Input: 12560
     Output: 3 ore, 29 minuti e 20 secondi.
 
+    var time = 0;
+    
+
   Consigli:
   In un'ora ci sono 60 minuti, in un minuto 60 secondi. Quindi quanti secondi ci sono in un'ora? ;)
 
